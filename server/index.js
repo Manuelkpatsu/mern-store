03f570db31171d1e0622f777d1f1535578449dc2
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-11-07 16:40:01 +0000

    Add socket.io

--- server/index.js
+++ server/index.js
@@ -3,13 +3,16 @@ const chalk = require('chalk');
 
 const app = require('./app')
 const keys = require('./config/keys');
+const socket = require('./socket');
 
 const { port } = keys;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(
         `${chalk.green('✓')} ${chalk.blue(
             `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
         )}`
     );
 });
+
+socket(server);
